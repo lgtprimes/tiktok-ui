0d70897ddbb90d48be319f8299d8bd444b439f46
giathanh <lgtprimes@gmail.com>
2025-11-08 15:21:14 +0700

    feat: setup layout structure and routing

--- eslint.config.js
+++ eslint.config.js
@@ -3,15 +3,20 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import react from 'eslint-plugin-react';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    plugins: {
+      react,
+    },
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettier, // 👈 thêm dòng này để tắt xung đột giữa ESLint và Prettier
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -24,6 +29,12 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
+      'react/jsx-wrap-multilines': ['error', {
+        return: 'parens-new-line',
+        arrow: 'parens-new-line',
+        condition: 'ignore',
+        declaration: 'ignore'
+      }],
+    }
   },
 ])
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fragment } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { publicRoutes } from '@/routes'
+import { DefaultLayout } from '@/components/Layout'
+
 
 function App() {
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component
 
-  return (
-    <div>
-      
-    </div>
-  )
+                        let Layout = DefaultLayout;
+                        if(route.layout) {
+                            Layout = route.layout
+                        } 
+                        else if (route.layout === null) {
+                            Layout = Fragment
+                        }
+
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        )
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    )
 }
 
 export default App
